f72b881c2c43dbdeabc955768f68cf106ab0228a
Pieroggo <greg2mazur@gmail.com>
2023-10-15 18:59:00 +0200

    added some tex articles, made first foundations for functions

---
+++ antiplagarism_tools.py
@@ -0,0 +1,14 @@
+#TODO:
+#1. function with string comparison
+#   - by paragraph
+#       * word threshold (eg. 20 in a row)
+#       * if eg. 1 in 8 chars are mismatched, it is still considered a match
+#   - by sentences (eg. 2 sentences matched means plagarism)
+#2. formula comparison
+#   - using trees to represent formulas
+#   - checking the formula structure - plagarised when formulas are the same with different variable names
+#3. different plagarism degrees
+#   - overt plagarism (very visible)
+#   - possible plagarism (not 100%, but has proof of possibility for plagarism)
+#   - vague plagarism (eg. sophisticated words, which happened in other works, every other word matched)
+
---
+++ document_handler.py
@@ -0,0 +1,5 @@
+#TODO:
+#1. basic file handler - to text
+#2. separation by backslash instructions
+#3. using imported method for checking plagarism
+#4. (BONUS) generate html report
