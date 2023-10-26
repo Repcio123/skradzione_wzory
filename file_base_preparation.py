2e6f2e2189b0b9c62ef0d395101167ea02f1917c
Pieroggo <greg2mazur@gmail.com>
2023-10-26 11:25:23 +0200

    modified 1 article, made an alpha version of paragraph extractor
    needs to also catch blocks of text which aren't in the paragraph statements

--- file_base_preparation.py
+++
@@ -1,7 +0,0 @@
-class TexParagraphExtractor:
-    def extract_paragraphs(self):
-        ...
-
-#TODO:
-#1. make a function to extract paragraphs/text chunks from article
-#2. make a function to extract formulas from article
