4b8feee0d8986bb16b46b497d248857e8d69da69
Pieroggo <greg2mazur@gmail.com>
2023-10-26 11:28:02 +0200

    made extract_paragraphs static

--- paragraph_extractor.py
+++ paragraph_extractor.py
@@ -2,12 +2,13 @@
 from TexSoup import TexSoup
 import os
 class TexParagraphExtractor:
-    def extract_paragraphs(self, tex_text):
+    @staticmethod
+    def extract_paragraphs(tex_text):
         paragraphs=[]
         soup=TexSoup(tex_text)
         for p in soup.find_all('paragraph'):
             paragraphs+=[p.string]
-        print(paragraphs)
+        return paragraphs
 tPE=TexParagraphExtractor()
 with open(os.getcwd()+"//tex_file_base//dummy_PCA.tex","r") as file:
     sampleText=file.read()
