02f4b301334c8ba2410008900c31c37465efeec1
BarRep <ketrabceper@gmail.com>
2024-01-15 16:14:14 +0100

    more tex files

--- tests/tex_file_base_test.py
+++ tests/tex_file_base_test.py
@@ -2,17 +2,14 @@ import os
 import json
 TEX_FOLDER = 'tex_file_base/tex'
 
+
 def get_documents_list() -> list[str]:
     return os.listdir(TEX_FOLDER)
+
 
 def is_min_tex_amount(files):
     assert len(files) >= 50, "minimal amount of documents is done"
 
 
-
-
-
-is_min_tex_amount(get_documents_list())
-
-
-
+if __name__ == "__main__":
+    is_min_tex_amount(get_documents_list())
