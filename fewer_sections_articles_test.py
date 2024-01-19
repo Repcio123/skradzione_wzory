5edd54476901b587903931b4bad99034f6a72e4d
BarRep <ketrabceper@gmail.com>
2024-01-19 14:48:08 +0100

    unique files test

--- fewer_sections_articles_test.py
+++ fewer_sections_articles_test.py
@@ -13,11 +13,6 @@ class FewerSections():
         self.testedDocumentMain4 = DocumentListHandler.initSoupFromTexFile('files_to_test/fewer_sections/main4/tex/main4.tex')
         self.docBaseMain4 = DocumentListHandler.init_tex_document_base(TEX_FOLDER_MAIN4)
 
-    def list_files_with_full_path(directory):
-        files = os.listdir(directory)
-        full_paths = [os.path.join(directory, file) for file in files]
-        return full_paths
-
     def compare_articles_by_hashes_test_laplace(self):
         antiPlagarism = AntiPlagarism(self.docBaseLaplace)
         results = antiPlagarism.compare_to_document_base(self.testedDocumentLaplace, AntiPlagarism.test_full_content_hashes)
@@ -68,15 +63,18 @@ class FewerSections():
         results = antiPlagarism.compare_to_document_base(self.testedDocumentMain4, AntiPlagarism.test_jaccard_distance)
         return {"main4 partial with full by jaccard": results[1].ratio}
     
+    def run_tests(self, path):
+        with open(path, "w") as f:
+            f.write(str(self.compare_articles_by_hashes_test_laplace()) + "\n")
+            f.write(str(self.compare_articles_by_hashes_test_main4()) + "\n")
+            f.write(str(self.compare_articles_by_chars_test_laplace()) + "\n")
+            f.write(str(self.compare_articles_by_chars_test_main4()) + "\n")
+            f.write(str(self.compare_articles_lavensthein_laplace()) + "\n")
+            f.write(str(self.compare_articles_lavensthein_main4()) + "\n")
+            f.write(str(self.compare_articles_cosine_laplace()) + "\n")
+            f.write(str(self.compare_articles_cosine_main4()) + "\n")
+            f.write(str(self.compare_articles_jaccard_laplace()) + "\n")
+
 if __name__ == "__main__":
     test = FewerSections()
-    with open("files_to_test/fewer_sections/test_results.txt", "w") as f:
-        f.write(str(test.compare_articles_by_hashes_test_laplace()) + "\n")
-        f.write(str(test.compare_articles_by_hashes_test_main4()) + "\n")
-        f.write(str(test.compare_articles_by_chars_test_laplace()) + "\n")
-        f.write(str(test.compare_articles_by_chars_test_main4()) + "\n")
-        f.write(str(test.compare_articles_lavensthein_laplace()) + "\n")
-        f.write(str(test.compare_articles_lavensthein_main4()) + "\n")
-        f.write(str(test.compare_articles_cosine_laplace()) + "\n")
-        f.write(str(test.compare_articles_cosine_main4()) + "\n")
-        f.write(str(test.compare_articles_jaccard_laplace()) + "\n")
+    test.run_tests("files_to_test/fewer_sections/test_results.txt")
